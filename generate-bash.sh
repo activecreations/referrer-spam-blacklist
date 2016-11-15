15638ef18774ddabe3e43f1da646f7205af509f1
Jürgen van Dijk <j.vandijk@activecreations.nl>
2016-11-15 13:25:50 +0100

    Update script

--- generate-bash.sh
+++ generate-bash.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Make sure file exists.
 touch referer_spam.conf
 
+# Empty out file.
+> referer_spam.conf
+
+# Loop spammers list and append a line for each host.
 sort spammers.txt | uniq | sed 's/\./\\\\./g' | while read host; 
 do 
     echo "if (\$http_referer ~ '$host') {return 403;}" >> referer_spam.conf
